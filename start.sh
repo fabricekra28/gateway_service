8fc0be9224018a3dc5456991b945fc3e04cccfdf
fabricekra28 <krafabrice64@gmail.com>
2025-08-24 21:31:05 +0000

    update start

--- start.sh
+++ start.sh
@@ -3,19 +3,19 @@
 echo "Waiting for backend services..."
 
 # On attend que users_service soit joignable
-while ! nc -z users 8001; do
+while ! nc -z users-service.microservices.svc.cluster.local 8001; do
   echo "Waiting for users_service..."
   sleep 2
 done
 
 # On attend que products_service soit joignable
-while ! nc -z products 8002; do
+while ! nc -z products-service.microservices.svc.cluster.local 8002; do
   echo "Waiting for products_service..."
   sleep 2
 done
 
 # On attend que orders_service soit joignable
-while ! nc -z orders 8003; do
+while ! nc -z orders-service.microservices.svc.cluster.local 8003; do
   echo "Waiting for orders_service..."
   sleep 2
 done
